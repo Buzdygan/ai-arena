6d285195d3d6d9ddf957969b3d2a7ad0a850a292
mk277558 <mk277558@students.mimuw.edu.pl>
2011-12-14 22:15:17 +0100

    added: 1) simple templates and views for sending files and receiving results 2) nadzorca is moved to contests app and uses its model 3) admin panel 4) database with sample objects 5) contents/models was modified

---
+++ ai_arena/contests/admin.py
@@ -0,0 +1,10 @@
+from contests.models import *
+from django.contrib import admin
+
+admin.site.register(Game)
+admin.site.register(Bot)
+admin.site.register(Contest)
+admin.site.register(MatchBotResult)
+admin.site.register(Match)
+admin.site.register(BotContestRanking)
+
