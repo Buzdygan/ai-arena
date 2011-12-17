9f71ea33f5b1f407d7cc45450d6e32081f3805bd
Jakub Tlałka <mamba57@gmail.com>
2011-12-17 12:09:16 +0100

    https://dev.mimuw.edu.pl/issues/1251

--- ai_arena/contests/views.py
+++ ai_arena/contests/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.http import Http404, HttpResponseRedirect
 from django.core.urlresolvers import reverse
-import nadzorca
+from nadzorca import nadzorca
 
 def index(request):
     return render_to_response('index.html')
--- ai_arena/settings_defaults.py
+++ ai_arena/settings_defaults.py
@@ -120,8 +120,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
+    'ai_arena.contests',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
