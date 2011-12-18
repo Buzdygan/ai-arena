ec2869b301b1b7ae4595e2c706c3ce7aaf54de9a
Jakub Tlałka <mamba57@gmail.com>
2011-12-18 23:19:35 +0100

    added test game and bots to fixtures

---
+++ ai_arena/media/game_judges_sources/test_game/test_judge.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+
+    const int BOARD_SIZE = 5;
+    string message, board = string(BOARD_SIZE, '0');
+    int move = 0;
+    bool player1 = false;
+
+    do
+    {
+        player1 = !player1;
+
+        if (player1)
+            message = "[1]";
+        else
+            message = "[2]";
+        message += board;
+
+        cerr << "sending message " << message << endl;
+        cout << message << "END" << endl;
+
+        cin >> move;
+        cerr << "received move " <<  move << endl;
+
+        if (player1)
+            board[move] = '1';
+        else
+            board[move] = '2';
+
+    } while (board[BOARD_SIZE-1] == '0');
+
+    if(player1)
+        cerr << "player 1 won " << endl;
+    else
+        cerr << "player 2 won" << endl;
+
+    cout << "[0] END" << endl;
+
+    return 0;
+}
