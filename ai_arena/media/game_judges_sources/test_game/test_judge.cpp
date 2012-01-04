5626847842c1a7d24d445a88bfad680dae9cc985
Szymon Majewski <szymon@szymon.(none)>
2012-01-04 20:44:35 +0100

    Nadzorca zwraca wynik rozgrywki

--- ai_arena/media/game_judges_sources/test_game/test_judge.cpp
+++ ai_arena/media/game_judges_sources/test_game/test_judge.cpp
@@ -34,12 +34,14 @@ int main(){
 
     } while (board[BOARD_SIZE-1] == '0');
 
+    cout << "[0] END" << endl
+
     if(player1)
         cerr << "player 1 won " << endl;
+        cout << "[1, 0] END" << endl;
     else
         cerr << "player 2 won" << endl;
-
-    cout << "[0] END" << endl;
+        cout << "[0, 1] END" << endl;
 
     return 0;
 }
