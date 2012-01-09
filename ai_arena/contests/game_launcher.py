957e5349d8fb7c48804cf8a0e43334483a667b86
Karol Zebrowski <kz277580@students.mimuw.edu.pl>
2012-01-09 23:11:50 +0100

    gearman employed - prowizorka

--- ai_arena/contests/game_launcher.py
+++ ai_arena/contests/game_launcher.py
@@ -1,6 +1,20 @@
 from nadzorca import nadzorca
 from decimal import Decimal
-from contests.models import Match, MatchBotResult 
+from contests.models import Match, MatchBotResult
+import gearman
+import pickle
+
+class PickleDataEncoder(gearman.DataEncoder):
+    @classmethod
+    def encode(cls, encodable_object):
+        return pickle.dumps(encodable_object)
+
+    @classmethod
+    def decode(cls, decodable_string):
+        return pickle.loads(decodable_string)
+
+class PickleClient(gearman.GearmanClient):
+    data_encoder = PickleDataEncoder
 
 def launch_single_match(game, bots):
     """
@@ -8,30 +22,7 @@ def launch_single_match(game, bots):
         with given list of bots
     """
 
-    bots2 = bots
-    bots = bots[:-1]
-    judge_program = game.judge_bin_file.path
-    time_limit = 2
-    memory_limit = 32
-    bots_programs = [bot.bot_bin_file.path for bot in bots]
+    arguments = {'game': game, 'bots': bots}
 
-    results = nadzorca.play(judge_file=judge_program, players=bots_programs,
-                    memory_limit=memory_limit, time_limit=time_limit)
-
-    """
-    bot_results = [MatchBotResult(score=res, bot=bot, time_used=10, memory_used=5) \
-                for score, res in zip(results, bots)]
-                """
-
-    match = Match(ranked_match=False, game=game, match_log="", time_limit=1000, memory_limit=32)
-    match.save()
-    results = results[1:-2].split(',')
-    print(results)
-    bots = bots2
-    for res, bot in zip(results, bots):
-        bot_result = MatchBotResult(score=Decimal(res), bot=bot, time_used=10, memory_used=5) 
-        bot_result.save()
-        match.players_results.add(bot_result)
-    match.save()
-
-
+    gearman_client = PickleClient(['localhost:4730'])
+    gearman_client.submit_job('single_match', arguments)
