f866419fcec04469f854af7f52785e94dcadf82c
Karol Zebrowski <kz277580@students.mimuw.edu.pl>
2012-05-09 02:53:44 +0200

    improved pok judge

--- ai_arena/nadzorca/testing/pok2/pok_judge.cpp
+++ ai_arena/nadzorca/testing/pok2/pok_judge.cpp
@@ -10,69 +10,60 @@
 
 #define ROUNDS_COUNT 200
 
-#define CHECK 1
+#define CALL 1
 #define FOLD 0
 
 using namespace std;
 
-string toSend1, toSend2;
-istringstream received1, received2;
+void error(int pl, string info){
+    cerr << "ERRRORR: player " << pl << " " << info << "\n";
+    cout << "[0]END\n";
+    if(pl == 1)
+        cout << "[0, 2]\n";
+    else
+        cout << "[2, 0]\n";
+}
 
-int actualBet=0;
+string toSend[3];
+int receivedMes[3];
 
-void toSend(int pl, int mes){
+int actualBet = 0;
+
+void buffer(int pl, int mes){
     stringstream ss;
     ss << mes;
-    if(pl == 1){
-        if(toSend1 != "")
-            toSend1 += " ";
-        toSend1 += ss.str();
-    }
-    if(pl == 2){
-        if(toSend2 != "")
-            toSend2 += " ";
-        toSend2 += ss.str();
-    }
+    if(toSend[pl] != "")
+        toSend[pl] += " ";
+    toSend[pl] += ss.str();
 }
 
-//TODO obsluga DEAD i return
-bool send(int pl){
-    string toSend, received;
-    if(pl == 1){
-        toSend = toSend1;
-        toSend1 = "";
-    }
-    if(pl == 2){
-        toSend = toSend2;
-        toSend2 = "";
-    }
-    cerr << "wys " << "[" << pl << "]" << toSend << "\n";
-    cout << "[" << pl << "]" << toSend << "\n";
+void send(int pl){
+    string mes, received;
+    mes = toSend[pl];
+    toSend[pl] = "";
+    cerr << "wys " << "[" << pl << "]" << mes << "\n";
+    cout << "[" << pl << "]" << mes << "\n";
     getline(cin, received);
     cerr << "dost " << received << "\n";
-    if(pl == 1){
-        received1.clear();
-        received1.str(received);
-    }
-    if(pl == 2){
-        received2.clear();
-        received2.str(received);
-    }
-    return true;
+    if(received == "_DEAD_")
+        error(pl, "dead");
+    else{
+        istringstream ss(received);
+        int num;
+        if((ss >> num).fail())
+            error(pl, "NaN");
+        else
+            receivedMes[pl] = num;
+    }            
 }
 
-//TODO obsluga return
-bool readMes(int pl, int * mes){
-    if(pl == 1)
-        return (received1 >> *mes);
-    if(pl == 2)
-        return (received2 >> *mes);
-}
-
-bool readBet(int player, int message, int * bet){
-    toSend(player, message);
-    send(player);
-    return readMes(player, bet);
+void readMes(int pl, int& mes, int min, int max, bool maybe0){
+    int num = receivedMes[pl];
+    if(((num >= min) && (num <= max)) || (maybe0 && (num == 0))){
+        mes = num;
+        receivedMes[pl] = -1;
+    }else
+        error(pl, "mes out of range");
 }
 
 int decisionMaker(int * bets){
@@ -87,44 +78,52 @@ int opponent(int player){
     return 3 - player;
 }
 
-bool readBets(int * bets, bool secondRound){
-    if(bets[1] == 0)
-        readMes(1, bets+1);
-    if(bets[2] == 0)
-        readMes(2, bets+2);
+bool readBets(int * bets, bool firstRound){
+    for(int pl=1; pl <= 2; pl++)
+        if(bets[pl] == 0)
+            readMes(pl, bets[pl], MIN_BET, MAX_BET, false);
+    for(int pl=1; pl <= 2; pl++)
+        buffer(opponent(pl), bets[pl]);
     int dm = decisionMaker(bets);
     if(dm){
         int op = opponent(dm);
-        toSend(op, bets[dm]);
-        readBet(dm, bets[op], bets);
-        if(bets[0] > 0){
-            bets[dm] = bets[0];
+            send(dm);
+        int response;
+        if(firstRound)
+            readMes(dm, response, MIN_BET, MAX_BET, true);
+        else
+            readMes(dm, response, 0, 1, true);
+        if(response > 0){
+            buffer(op, CALL);
+            if(firstRound){
+                send(op);
+                bets[dm] = response;
+            }
             actualBet += bets[op];
-            toSend(op, CHECK);
-            if(!secondRound)
-            send(op);
             bets[op] = 0;
             bets[0] = 0;
         }else{
             actualBet += bets[dm];
-            toSend(op, FOLD);
+            buffer(op, FOLD);
             bets[0] = op;
         }
     }else{
         actualBet += bets[1];
         bets[0] = 0;
-        toSend(1, bets[2]);
-        toSend(2, bets[1]);
-        bets[1] = bets[2] = 0;
-        if(!secondRound){
-        send(1);
-        send(2);
-        }
+        for(int pl=1; pl <= 2; pl++)
+            bets[pl] = 0;
+        if(firstRound)
+            for(int pl=1; pl <= 2; pl++)
+                send(pl);
     }
     return true;
 }
 
 int main(){
+    for(int i=0; i < 3; i++){
+        toSend[i] = "";
+        receivedMes[i] = -1;
+    }
     int bets[3];
     int scores[3];
     int pl1card=0,
@@ -139,11 +138,10 @@ int main(){
     for(int i=0; i<ROUNDS_COUNT; i++){
         pl1card = rand() % CARD_COUNT;
         pl2card = rand() % CARD_COUNT;
-        bets[0] = bets[1] = bets[2] = 0;
-        //pl1card = 0;
-        //pl2card = 1;
-        toSend(1, pl1card);
-        toSend(2, pl2card);
+        for(int i=0; i < 3; i++)
+            bets[i] = 0;
+        buffer(1, pl1card);
+        buffer(2, pl2card);
         cerr << "sending CARDS " << pl1card << " " << pl2card << "\n";
         send(1);
         send(2);
@@ -155,18 +153,17 @@ int main(){
             winner = 1;
         if(pl2card > pl1card)
             winner = 2;
-        //cerr << "first BET\n";
-        readBets(bets, false);
+        readBets(bets, true);
         if(bets[0])
             winner = bets[0];
         else{
             cerr << "second BET\n";
-            readBets(bets, true);
+            readBets(bets, false);
             if(bets[0])
                 winner = bets[0];
             else{
-                toSend(1, pl2card);
-                toSend(2, pl1card);
+                buffer(1, pl2card);
+                buffer(2, pl1card);
             }
         }
         cerr << "winner " << winner << " actBet " << actualBet << "\n";
