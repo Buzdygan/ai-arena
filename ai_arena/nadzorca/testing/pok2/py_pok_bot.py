11eeef912119bbae570184f5bbe07d4c7ad693df
Karol Zebrowski <kz277580@students.mimuw.edu.pl>
2012-05-14 18:55:07 +0200

    debugged python bot

--- ai_arena/nadzorca/testing/pok2/py_pok_bot.py
+++ ai_arena/nadzorca/testing/pok2/py_pok_bot.py
@@ -24,15 +24,24 @@ def decision(myBet, opBet, card):
         return 1
     if card > 2:
         return 1
+    return 0
+
+receivedInts = []
+
+def takeInt():
+    global receivedInts
+    if len(receivedInts) == 0:
+        receivedInts = map(int, raw_input().split())
+    return receivedInts.pop(0)
 
 
 while True:
-    card = int(stdin.readline()[:-1])
+    card = takeInt()
     stderr.write("CARD " + str(card) + "\n")
     my1bet = firstBet(card)
     stdout.write("" + str(my1bet) + "\n")
     stdout.flush()
-    op1bet = int(stdin.readline()[:-1])
+    op1bet = takeInt()
     stderr.write("op1bet " + str(op1bet) + "\n")
     if op1bet > my1bet:
         decision1 = decision(my1bet, op1bet, card)
@@ -42,7 +51,7 @@ while True:
             stdout.flush()
             continue
     if my1bet > op1bet:
-        decision1 = int(stdin.readline()[:-1])
+        decision1 = takeInt()
         stderr.write("op made decision " + str(decision1) + "\n")
         if decision1 == 0:
             continue
@@ -54,7 +63,7 @@ while True:
     my2bet = secondBet(my1bet, op1bet, card)
     stdout.write(str(my2bet) + "\n")
     stdout.flush()
-    op2bet = int(stdin.readline()[:-1])
+    op2bet = takeInt()
     stderr.write("op2bet " + str(op2bet) + "\n")
     if op2bet > my2bet:
         decision2 = decision(my1bet+my2bet, op1bet + op2bet, card)
@@ -64,11 +73,11 @@ while True:
         if decision2 == 0:
             continue
     if my2bet > op2bet:
-        decision2 = int(stdin.readline()[:-1])
+        decision2 = takeInt()
         stderr.write("op made decision " + str(decision2) + "\n")
         if decision2 == 0:
             continue
     if my2bet == op2bet:
         stderr.write("equal 2bet\n")
-    opCard = int(stdin.readline()[:-1])
+    opCard = takeInt()
 
